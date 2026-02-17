8c78f2b239864bf7c821437bd8a6e9efeda0227c
Nimanthika <nimanthikaabeyrathna@gmail.com>
2026-02-17 21:19:35 +0530

    initialize the index.php

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BusLynk</title>
+    <link rel="shortcut icon" href="./image/favicon.png" type="image/png">
+    <link rel="stylesheet" href="./css/style.css">
+   
+</head>
+<body>
+    <h1>hello</h1>
 
-
-echo "Hello world";
-
-?>
+    
+</body>
+</html>
